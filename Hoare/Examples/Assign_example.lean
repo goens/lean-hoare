b5ddd0316d23c122be512fe18ae8302d401a3d6b
dhyan-aranha <dhyan.aranha@gmail.com>
2025-04-24 20:38:13 +0200

    cleaned up Assign_example more

--- Hoare/Examples/Assign_example.lean
+++ Hoare/Examples/Assign_example.lean
@@ -11,18 +11,11 @@ def foo : Triple :=
 X := X + 1
 {X == 2}
 
-def foo_com : Com := [com| X := X + 1]
-
-def foo_expr : Expr := [expr| X + 1]
-
-def foo_stmt : Statement := Statement.atom [expr| X == 2]
-
-theorem foo_match  : Statement.subst "X" foo_expr foo_stmt = [stmt| (X + 1) == 2] := by
-  simp [foo_expr, foo_stmt, Statement.subst]
-  simp [Expr.subst]
-
 -- Here we use TripleHolds.assign' I had some inference problems getting TripleHolds.assign
 -- to work.
 theorem foo_holds' : TripleHolds foo := by
-  have := TripleHolds.assign' foo_stmt foo_expr "X" foo_com
-  exact this
+  let foo_stmt := foo.3
+  let foo_com := foo.2
+  let foo_expr := [expr| X + 1]
+  apply TripleHolds.assign' foo_stmt foo_expr "X" foo_com
+
